55c95722d5a75b858055ce481661b5a6a10ff4fc
benzinofam <benzinofam@debianHomeLaptop>
2016-10-11 22:15:33 -0400

    progress

--- route.py
+++ route.py
@@ -1,12 +1,12 @@
 import netifaces, socket
-
+ETH_P_ALL = 3
 def main():
     #get list of interfaces or something
     if netifaces.interfaces() == -1:
         print("getifaddrs")
         return 1
     ifaces = netifaces.interfaces()
-    
+
     #Get the list of interfaces
     for iface in ifaces:
     #Check if this is a packet address, there will be one per
@@ -14,16 +14,26 @@ def main():
     #about those for the purpose of enumerating interfaces. We can
     #use the AF_INET addresses in this list for example to get a list
     #of our own IP addresses
-        print iface
+        print("Interface name:", iface[3:])
         addrs = netifaces.ifaddresses(iface)
-        print addrs[netifaces.AF_INET]
+        interface = addrs[netifaces.AF_INET]
+        print("***************************************************************")
+        #IP address
+        print interface[0]['addr']
+        print("***************************************************************")
+        print("Creating socket...")
     #if(tmp->ifa_addr->sa_family==AF_PACKET
-        packet_socket = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.htons(ETH_P_ALL))
-        if packet_socket < 0:
-            print("error creating socket")
-        packet_socket.bind(("eth1", 0))
-        print("socket bound")
-        print("socket created")
+        try:
+            packet_socket = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, socket.htons(ETH_P_ALL))
+            print("Socket created...")
+            packet_socket.bind(interface[0])
+            print("Socket bound...\n")
+
+            if packet_socket < 0:
+                print("error creating socket")
+            #print("socket bound")
+        except socket:
+            print("Something went wrong creating socket")
     #s = socket.socket(socket.AF_PACKET, socket.SOCK_RAW, htons(ETH_P_ALL));
     #print("socket created")
 main()
